4edaf2570de80f6ea95710317eeff6d500744ba0
iamabdurrasheed <22bd1a0538@gmail.com>
2024-11-14 12:19:45 +0530

    Committed sample

---
+++ src/main/java/com/app/demo/JavaMavenExample/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.JavaMavenExample;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("ROLL NO:22BD1A0538");
+        System.out.println("NAME:MOHAMMED ABDUR RASHEED");
+        
+    }
+}
